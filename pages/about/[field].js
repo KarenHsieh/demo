5c83df0b0895a57dfa6693300f651ea19249aa10
Karen Hsieh <karenhsieh@eztravel.com.tw>
2020-10-13 16:43:11 +0800

    測試getStaticProps和getStaticPaths

--- pages/about/[field].js
+++ pages/about/[field].js
@@ -1,42 +1,48 @@
 
 import path from 'path'
-const aboutDirectory = path.join(process.cwd(), 'about')
+// const aboutDirectory = path.join(process.cwd(), 'about')
 
-function PrintField({content}) {
+function PrintField({fieldValue}) {
+
   return (
     <div>
       <h5>印出欄位內容</h5>
-      {
-        for (var data in content) {
-          <section>
-            <p>Json Content = {Json.stringify(data)}</p>
-          </section>
-        }
-      }
+      <section>
+        <p>{JSON.stringify(fieldValue)}</p>
+      </section>
     </div>
   )
 }
 
 export async function getStaticPaths() {
+  
+  const infos = await getData();
+  const fields = Object.getOwnPropertyNames(infos);
 
-  const res = await fetch('https://randomuser.me/api/');
-  let infos = await res.json()
-  infos = JSON.parse(JSON.stringify(infos)).results[0]
+  const paths = fields.map((value) => ({
+    params: { field: value },
+  }))
 
-
-  const paths = infos.forEach((info) => `/about/${info.field}`)
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
   return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
-  const fullPath = path.join(aboutDirectory, params.field)
-  const res = await fetch(fullPath);
-  const content = await res.json();
   
-  return { props: { content } }
+  const infos = await getData();
+  
+  return { 
+    props: {
+      fieldValue: infos[params.field]
+    } 
+  }
+}
+
+async function getData() {
+  const res = await fetch('https://randomuser.me/api/');
+  
+  let infos = await res.json()
+  infos = JSON.parse(JSON.stringify(infos)).results[0]
+  return infos;
 }
 
 export default PrintField
